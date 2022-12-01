05606b2dbfcd5a0891318be621e39c3e28d8e13e
jungle <minuet1215@jbnu.ac.kr>
2022-12-01 18:45:41 +0900

    Feat: Member class, MemberService(가입, 조회) 구현

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+// 관례상, 인터페이스에 대한 구현체가 하나일 경우, 이름 뒤에 'Impl'을 붙인다.
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
