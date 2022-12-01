5ab0f966c8a066f329fc9316eeca9619b74c1f75
jungle <minuet1215@jbnu.ac.kr>
2022-12-02 04:19:58 +0900

    Feat: AppConfig를 스프링 기반으로 변경

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,15 +5,17 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
-        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        Member member = new Member(memberId, "memberVIP", Grade.VIP);
         memberService.join(member);
 
         Order order = orderService.createOrder(1L, "itemA", 10000);
