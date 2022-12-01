c5f1721b4528f6676f231e0da28d7534a2758426
jungle <minuet1215@jbnu.ac.kr>
2022-12-02 02:06:43 +0900

    Chore: 함수명 오타 수정

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FIxDiscountPolicy implements DiscountPolicy {
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private int DISCOUNT_FIX_AMOUNT = 1_000;
 
